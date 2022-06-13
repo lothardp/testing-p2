bcace796e97bf6feb0040a470605d4d0fcfc2601
fdduarte <fdduarte@uc.cl>
2022-06-13 16:17:01 -0400

    ft: added flight and reservation models unit test

--- test/models/flight_test.rb
+++ test/models/flight_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 class FlightTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Crear un vuelo sin datos" do
+    flight = Flight.new
+    assert_not flight.save
+  end
+
+  test "Crear un vuelo con datos" do
+    flight = Flight.new({ datetime: Time.now, origin: 'NYC', destination: 'BCN'})
+    assert flight.save
+  end
+
+  test "Vuelos futuros" do
+    assert_equal Flight.all_future.length, 1
+  end
+
+  test "Porcentaje de ocupación" do
+    assert_equal flights(:one).occupation_percentage, 0.83
+    assert_equal flights(:two).occupation_percentage, 0
+  end
+
+  test "Asientos usados" do
+    assert_equal flights(:one).taken_seats, ['1A']
+    assert_equal flights(:two).taken_seats, []
+  end
+
+  test "Asiento disponible" do
+    assert flights(:one).free_seat '1B'
+    assert_not flights(:one).free_seat '1A'
+  end
+
+  test "Asiento valido" do
+    assert flights(:one).valid_seat '1A'
+    assert_not flights(:one).valid_seat '1Z'
+  end
 end
--- test/models/reservation_test.rb
+++ test/models/reservation_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 class ReservationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Crear una reserva sin datos" do
+    res = Reservation.new()
+    assert_not res.save()
+  end
+
+  test "Crear una resevra" do
+    flight = flights(:one)
+    res = Reservation.new({ flight_id: flight.id, user: 'Test user', seat: '1B' })
+    assert res.save()
+  end
+
+  test "Crear una reserva con asiento usado" do
+    flight = flights(:one)
+    res = Reservation.new({ flight_id: flight.id, user: 'Test user', seat: '1A' })
+    assert_not res.save()
+  end
 end
